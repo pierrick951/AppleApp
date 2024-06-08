d3c726285d317aba122ac3ff8ea7e8df693d9fb7
Pierrick Ber <bernard.pierrick0@gmail.com>
2024-06-08 06:51:23 -0400

    first commit appleApp

---
+++ testredux/src/App.jsx
@@ -0,0 +1,21 @@
+export default function App() {
+  return (
+    <div className="main">
+       <div className="container">
+         <h2> 🍎 Pomme : 5</h2>
+       </div>
+       <div className="bodyApp">
+           <img src="https://www.plantes-et-sante.fr/images/istock-155375750.jpg" alt="" />
+       </div>
+       
+         <div className="boxButton">
+           <button>Ajouter</button>
+           <button>Ajouter X10</button>
+           <button>Suprimer</button>
+           <button>Suprimer x10</button>
+         </div>
+       
+    </div>
+
+  )
+}
